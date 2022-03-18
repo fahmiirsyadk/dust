d6cdb9313b53bb7b88559a8883dedfe20c660f70
fahmiirsyadk <fahmiirsyad10@gmail.com>
2022-03-18 09:01:58 +0700

    add transform template & test

--- src/Engine/Engine.res
+++ src/Engine/Engine.res
@@ -1,6 +1,8 @@
 open Promise
 @module external fsglob: array<string> => Promise.t<array<string>> = "fast-glob"
 @scope("Object") @val external obj_entries: 'a => array<'a> = "entries"
+@val external importJs: string => 'a = "require"
+@module("fs-extra") external pathExists: string => Promise.t<bool> = "pathExists"
 
 type metaTemplate = {
   status: bool,
@@ -17,5 +19,69 @@ let globalMetadata: array<{.}> = []
 let pagesPath = [Config.getFolderBase(), "pages"]->Node.Path.join->Node.Path.normalize
 let pagePattern = [pagesPath, "**", "*.js"]->Node.Path.join->Node.Path.normalize
 
-
+// global functions
 let cleanOutputFolder = () => Utils.emptyDir(Config.getFolderOutput())
+let deleteAllCache = %raw("
+  function() {
+    Object.keys(require.cache).forEach(function(key) {
+      delete require.cache[key]
+    })
+  }
+")
+
+type templateData<'a> = {main: option<'a>}
+
+type templateMetadata<'b> = {
+  status: bool,
+  content: option<'b>,
+}
+
+let transformTemplate = (originPath, props) => {
+  let basename = Node.Path.basename(originPath)->Js.String2.slice(~from=0, ~to_=-3)
+  let renderer = (originPath, props) => {
+    let data = importJs(originPath)
+
+    let getStatus = data.main->Belt.Option.flatMap(func => func)
+
+    switch getStatus {
+    | Some(val) => val(props)
+    | None => {
+        Utils.ErrorMessage.logMessage(#info(`main function in [${basename}] not exist`))
+        None
+      }
+    }
+  }
+
+  let checkFile = path => {
+    pathExists(path)
+    ->then(res => res->resolve)
+    ->catch(_ => {
+      Utils.ErrorMessage.logMessage(#error(`template [${basename}] not exist`))
+      false->resolve
+    })
+  }
+
+  checkFile(originPath)
+  ->then(res => res->resolve)
+  ->then(res => {
+    switch res {
+    | true =>
+      switch renderer(originPath, props) {
+      | Some(val) => {status: true, content: val}->resolve
+      | None => {status: false, content: None}->resolve
+      }
+    | false => {status: false, content: None}->resolve
+    }
+  })
+  ->catch(err => {
+    switch err {
+    | JsError(obj) => switch Js.Exn.message(obj) {
+      | Some(msg) => Utils.ErrorMessage.logMessage(#error(`Something error happen: ${msg}`))
+      | None =>
+        Utils.ErrorMessage.logMessage(#error(`Something error happen, must be non-error value`))
+      }
+    | _ => Utils.ErrorMessage.logMessage(#error(`Something unkown error happen`))
+    }
+    {status: false, content: None}->resolve
+  })
+}
--- test/Templates/TemplateTest.test.js
+++ test/Templates/TemplateTest.test.js
@@ -3,30 +3,58 @@ const fs = require("fs");
 const path = require("path");
 
 test("[Template] render basic template", async (t) => {
-  const { main } = require("./source/Template");
-  t.is(main(), "<!DOCTYPE html><html><body><h1>TEST</h1></body></html>");
+  const { transformTemplate } = require("./../../src/Engine/Engine");
+  let data = await transformTemplate(
+    path.join(__dirname, "source", "Template.js"),
+    {}
+  );
+  t.deepEqual(data, {
+    status: true,
+    content: "<!DOCTYPE html><html><body><h1>TEST</h1></body></html>",
+  });
 });
 
 test("[Template] render props template", async (t) => {
-  const { main } = require("./source/TemplateProps");
+  const { transformTemplate } = require("./../../src/Engine/Engine");
   const animals = ["cat", "dog", "bird"];
-  t.is(
-    main(animals),
-    "<!DOCTYPE html><html><body><h1>cat</h1><h1>dog</h1><h1>bird</h1></body></html>"
+  let data = await transformTemplate(
+    path.join(__dirname, "source", "TemplateProps.js"),
+    animals
   );
+  t.deepEqual(data, {
+    status: true,
+    content:
+      "<!DOCTYPE html><html><body><h1>cat</h1><h1>dog</h1><h1>bird</h1></body></html>",
+  });
 });
 
 test("[Template] render component template", async (t) => {
-  const { main } = require("./source/TemplateComponent");
-  t.is(
-    main(),
-    "<!DOCTYPE html><html><head><title>test</title></head><body><h1>another comp</h1></body></html>"
+  const { transformTemplate } = require("./../../src/Engine/Engine");
+  let data = await transformTemplate(
+    path.join(__dirname, "source", "TemplateComponent.js"),
+    {}
   );
+  t.deepEqual(data, {
+    status: true,
+    content:
+      "<!DOCTYPE html><html><head><title>test</title></head><body><h1>another comp</h1></body></html>",
+  });
 });
 
-// test("[Template] render update template changes", (t) => {
-//   t.is(
-//     main(),
-//     "<!DOCTYPE html><html><head><title>test</title></head><body><h1>another comp updated</h1></body></html>"
-//   );
-// });
+test("[Template] should fail empty template", async (t) => {
+  const { transformTemplate } = require("./../../src/Engine/Engine");
+  let data = await transformTemplate(
+    path.join(__dirname, "source", "TemplateEmpty.js"),
+    {}
+  );
+  t.deepEqual(data, { status: false, content: undefined });
+});
+
+test("[Template] should throw error non exist template file", async (t) => {
+  const { transformTemplate } = require("./../../src/Engine/Engine");
+  let data = await transformTemplate(
+    path.join(__dirname, "source", "TemplateNonExist.js"),
+    {}
+  );
+  t.deepEqual(data, { status: false, content: undefined });
+});
